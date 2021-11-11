30c76c5e14ec2a473c259fc76e8e3a94145faada
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-11-11 08:23:49 +0600

    Добавьте файлы проекта.

---
+++ Lab8_WpfApp/MyCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab8_WpfApp
+{
+    class MyCommands
+    {
+        public static RoutedUICommand Close { get; set; }
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.A, ModifierKeys.Control, "Ctrl+A"));
+            Close = new RoutedUICommand("Close", "Close", typeof(MyCommands), inputs);
+        }
+    }
+}
